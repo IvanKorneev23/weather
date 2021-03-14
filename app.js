1fba6665609da161de5c8197b2e9034a20672db4
unknown <ivankorpop@mail.ru>
2021-03-14 19:43:02 +0300

    init

---
+++ app.js
@@ -0,0 +1,26 @@
+const dayWeatherData = {
+    morning: {
+        name: 'Утро',
+        temperature: 19,
+        precipitation: 'Нет',
+        icon: 'icons/good-weather.png'
+    },
+    day: {
+        name: 'День',
+        temperature: 15,
+        precipitation: 'Да',
+        icon: 'icons/smth-rain.png'
+    },
+    evening: {
+        name: 'Вечер',
+        temperature: 7,
+        precipitation: 'Да',
+        icon: 'icons/rain.png'
+    },
+    night: {
+        name: 'Ночь',
+        temperature: 5,
+        precipitation: 'Нет',
+        icon: 'icons/night.png'
+    }
+}
